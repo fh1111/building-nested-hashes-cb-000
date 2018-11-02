d155b5dd144677f329cad39239d81aacfaa47644
Fer Hernandez <fh.1111@gmail.com>
2018-11-02 01:56:16 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,17 +1,6 @@
 def third_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :here => {name: "LRome", age: "15", status: "alive"},
-      :hero_friends => []
-    },
-    :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :hero_friends => {}
-    }
+
   }
 
 
